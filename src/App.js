cf6ff29e61a0b15687449dd39a72c53a01c52637
Solomon Darko <kobbydarko2016@gmail.com>
2023-04-22 00:09:49 +0000

    second commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+//import './App.css';
+import img1 from './1.png'
+import './styles.css'
+
+
+
+
+
+
+
+
+
+
+  let solo = ( 
+  <div>
+
+    <h1 className='heading'>Red Red</h1>
+  <p className='redred'>
+
+      Red red is a popular Ghanaian stew consisting of beans, 
+      red palm oil, tomato paste, and flavorings such as garlic, 
+      ginger, chili, and onions. Some cooks like to add meat or 
+      fish broth into the stew, but it is completely optional. 
+      The stew is traditionally served with fried plantains on the side.
+</p>
+
+      <img src='https://agameals.com/wp-content/uploads/2022/05/maxresdefault-6.jpg' className="image"></img>
+      <img src='https://images.bolt.eu/store/2021/2021-04-07/2260e099-eb61-4330-99aa-e3b35625ee20.jpeg' className="image"></img>
+</div>)
+
+function App() {
+  return (
+    <div>
+      {solo}
+    </div>
+
+  );
+}
+
+
+/*function images (){
+    <div>
+      <img src='.1/png' alt=''/>
+      </div>
+}*/
+export default App;
